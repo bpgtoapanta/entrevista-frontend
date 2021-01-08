1db9d87aab2490dc2d002cbfac9de34fc2d7d8a3
ByrPato <bpgtoapanta@gmail.com>
2021-01-07 19:26:14 -0500

    Solucion de stock articulos resta disponibles, no deja ingresar si tiene stock 0

--- src/app/pages/orden/orden-edicion/orden-edicion.component.ts
+++ src/app/pages/orden/orden-edicion/orden-edicion.component.ts
@@ -9,6 +9,7 @@ import { ClienteService } from 'src/app/_service/cliente.service';
 import { OrdenService } from 'src/app/_service/orden.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DetalleOrden } from 'src/app/_model/detalleOrden';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-orden-edicion',
@@ -17,7 +18,6 @@ import { DetalleOrden } from 'src/app/_model/detalleOrden';
 })
 export class OrdenEdicionComponent implements OnInit {
 
-  form: FormGroup;
   clientes: Cliente[] = [];
   articulos: Articulo[] = [];
 
@@ -26,6 +26,7 @@ export class OrdenEdicionComponent implements OnInit {
   clienteSeleccionado: Cliente;
   articuloSeleccionado: Articulo;
 
+
   idClienteSeleccionado: number;
   idArticuloSeleccionado: number;
 
@@ -33,18 +34,13 @@ export class OrdenEdicionComponent implements OnInit {
 
   articulosSeleccionados: Articulo[] = [];
 
+
   filteredOptionsCliente: Observable<any[]>;
 
   constructor(private ordenService: OrdenService, private clienteService: ClienteService, private articuloService: ArticuloService,
     private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-    this.form = new FormGroup({
-      'cliente': new FormControl(),
-      'fecha': new FormControl(new Date()),
-    });
-
-
     this.listarClientes();
     this.listarArticulos();
   }
@@ -71,13 +67,30 @@ export class OrdenEdicionComponent implements OnInit {
         this.articuloService.obtenerPorId(this.idArticuloSeleccionado).subscribe(data => {
           articulo.nombre = data.nombre;
           articulo.codigo = data.codigo;
-          this.articulosSeleccionados.push(articulo);
+          articulo.valorUnitario = data.valorUnitario;
+          articulo.cantidad = 0;
+          articulo.stock = data.stock;
+          if (data.stock > 0) {
+            this.articulosSeleccionados.push(articulo);
+          } else {
+            this.mensaje = 'No se puede agregar el articulo stock en 0';
+            this.snackBar.open(this.mensaje, "Aviso", { duration: 2000 });
+          }
         });
       }
     } else {
       this.mensaje = 'Debe agregar un articulo';
       this.snackBar.open(this.mensaje, "Aviso", { duration: 2000 });
     }
+  }
+
+  diferenciaStock(articulo: Articulo){
+    this.articulosSeleccionados.forEach((art) => {
+      if (art.idArticulo == articulo.idArticulo) {
+        art.cantidad = articulo.cantidad;
+      }
+    });
+
   }
 
   removerArticulo(index: number) {
@@ -89,32 +102,39 @@ export class OrdenEdicionComponent implements OnInit {
   aceptar(){
     let cliente = new Cliente();
     cliente.idCliente = this.idClienteSeleccionado;
-
+    let tieneStock = true;
     let orden = new Orden();
     orden.cliente = cliente
-
-    let tzoffset = (new Date(this.fechaSeleccionada)).getTimezoneOffset() * 60000;
-    let localISOTime = (new Date(Date.now() - tzoffset)).toISOString();
-    debugger;
-    orden.fecha = localISOTime;
+    orden.fecha = moment(this.fechaSeleccionada).toISOString();
     let detalleOrden = [];
+    debugger;
     this.articulosSeleccionados.forEach(articulo => {
       let detalle = new DetalleOrden();
       detalle.articulo = articulo;
-      detalleOrden.push(detalle);
+      if (articulo.stock < articulo.cantidad) {
+        tieneStock = false;
+      } else {
+        detalle.articulo.stock = articulo.stock - articulo.cantidad;
+        detalleOrden.push(detalle);
+      }
     });
 
     orden.detalleOrden = detalleOrden;
-
-    this.ordenService.registrar(orden).subscribe(() => {
-      this.snackBar.open("Se registró", "Aviso", { duration: 2000 });
-      this.ordenService.listar().subscribe(data => {
-        this.ordenService.ordenCambio.next(data);
+    if (tieneStock) {
+      this.ordenService.registrar(orden).subscribe(() => {
+        this.snackBar.open("Se registró", "Aviso", { duration: 2000 });
+        this.ordenService.listar().subscribe(data => {
+          this.ordenService.ordenCambio.next(data);
+        });
+        setTimeout(() => {
+          this.limpiarControles();
+        }, 1000);
       });
-      setTimeout(() => {
-        this.limpiarControles();
-      }, 1000);
-    });
+    } else {
+      detalleOrden = [];
+      this.snackBar.open("El cantidad del stock de uno de los articulos es mayor a la disponible", "Aviso", { duration: 2000 });
+    }
+
 
   }
 
